c68f13177e4a5f3b0b9c7628d15454e6a179fe74
Donald Oakes <donald.h.oakes@gmail.com>
2020-10-05 18:52:50 -0600

    remove node dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,22 @@
-// TODO: rollup/bundling is not used due to this issue:
+// TODO: stay on plugin-typescript 3.x pending this issue:
 // https://github.com/rollup/plugins/issues/287
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
-    // browser-friendly UMD build
+    // browser UMD build (tsc builds module output & types)
     {
-        input: 'src/main.js',
+        input: 'src/main.ts',
         output: {
             name: 'flowbee',
-            file: pkg.browser,
+            file: 'dist/index.js',
             format: 'umd'
         },
         plugins: [
             resolve(),
-            commonjs()
+            commonjs(),
+            typescript()
         ]
-    },
-    {
-        input: 'src/main.js',
-        output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
-		]
     }
 ];
--- src/specifier.ts
+++ src/specifier.ts
@@ -1,7 +1,3 @@
-import * as fs from 'fs';
-import * as glob from 'glob';
-import * as jsYaml from 'js-yaml';
-
 export interface Specifier {
     id: string;
     label: string;
@@ -11,7 +7,6 @@ export interface Specifier {
 }
 
 export class FlowSpecifier implements Specifier {
-
     constructor(
         readonly id: string,
         readonly label: string,
@@ -19,28 +14,4 @@ export class FlowSpecifier implements Specifier {
         readonly category?: string,
         readonly layout?: object
     ) { }
-
-     /**
-      * Read specifiers from JSON or YAML files
-      * TODO: base can be a URL
-      * @param base base location
-      * @param pattern glob pattern for spec files
-      */
-    static async read(base: string, pattern = '**/*.spec'): Promise<Specifier[]> {
-        const specs: Specifier[] = [];
-        const files = glob.sync(pattern, { cwd: base });
-        for (const file of files) {
-            try {
-                const text = fs.readFileSync(file, 'utf-8');
-                if (text.startsWith('{')) {
-                    specs.push(JSON.parse(text));
-                } else {
-                    specs.push(jsYaml.safeLoad(text, { filename: file }));
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        return specs;
-    }
 }
